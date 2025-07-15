cb292f36ee1a21167241d126d991c06b2d5d113e
Dalton Rose <daltonjayrose@gmail.com>
2025-07-15 14:18:42 -0400

    Initial Commit

---
+++ scenes/player/player.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+func _process(delta):
+	
+	# Input
+	var direction = Input.get_vector("left", "right", "up", "down")
+	print(direction)
+	position += direction * 500 * delta
+	
+	# Laser Shooting Input
+	if Input.is_action_pressed("primary_action"):
+		print("Firing")
+		
+	# Grenade Throwing Input
+	if Input.is_action_pressed("secondary_action"):
+		print("Grenade Out!")
