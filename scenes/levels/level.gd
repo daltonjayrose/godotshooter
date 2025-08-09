38fc7c1adde86a381c43f01644b69cf5920f198c
Dalton Rose <daltonjayrose@gmail.com>
2025-08-09 18:29:17 -0400

    Added custom signals for gate, laser, and grenade.

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -1,1 +1,14 @@
 extends Node2D
+
+
+func _on_gate_player_entered_gate(body) -> void:
+	print("Player has entered gate.")
+	print(body)
+
+
+func _on_player_laser() -> void:
+	print("Laser from level")
+
+
+func _on_player_grenade() -> void:
+	print("Grenade from level")
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,5 +1,10 @@
 extends CharacterBody2D
 
+var can_laser: bool = true
+var can_grenade: bool = true
+signal laser
+signal grenade
+
 func _process(_delta):
 	
 	# Input
@@ -9,9 +14,24 @@ func _process(_delta):
 	
 	
 	# Laser Shooting Input
-	if Input.is_action_pressed("primary_action"):
-		print("Firing")
+	if Input.is_action_pressed("primary_action") and can_laser:
+		print("Firing!")
+		laser.emit()
+		can_laser = false
+		$LaserTimer.start()
+		
 		
 	# Grenade Throwing Input
-	if Input.is_action_pressed("secondary_action"):
+	if Input.is_action_pressed("secondary_action") and can_grenade:
 		print("Grenade Out!")
+		grenade.emit()
+		can_grenade = false
+		$GrenadeTimer.start()
+
+
+func _on_laser_timer_timeout() -> void:
+	can_laser = true
+
+
+func _on_grenade_timer_timeout() -> void:
+	can_grenade = true
